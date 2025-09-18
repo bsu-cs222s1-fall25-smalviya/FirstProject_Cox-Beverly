d2f1849d6853f08feacd7b47fa54b0cbfdafa1b9
vcc121 <vaughn.cox@bsu.edu>
2025-09-18 10:24:10 -0400

    Added test and WikipediaConnection class from demo

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.jayway.jsonpath:json-path:2.8.0")
+    implementation("net.minidev:json-smart:2.5.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
 }
 
 tasks.test {
