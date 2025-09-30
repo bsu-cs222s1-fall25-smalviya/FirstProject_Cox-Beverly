0e0c2ac0870353b63fa131b04b70275edb29b0ad
vcc121 <vaughn.cox@bsu.edu>
2025-09-30 11:38:49 -0400

    Changed RevisionFormatter to make timestamps more readable

--- src/main/java/bsu/edu/cs222/RevisionFormatter.java
+++ src/main/java/bsu/edu/cs222/RevisionFormatter.java
@@ -1,20 +1,26 @@
 package bsu.edu.cs222;
-
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 // Uses StringBuilder to format revisions
 
 public class RevisionFormatter {
+
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm:ss")
+                    .withZone(ZoneId.systemDefault());
+
     public static String format(List<Revision> revisions) {
         StringBuilder sb = new StringBuilder();
-        for (Revision r : revisions) {
-            sb.append("Revision by ")
-                    .append(r.getUser())
-                    .append(" at ")
-                    .append(r.getTimestamp())
+        for (Revision rev : revisions) {
+            String formattedTime = FORMATTER.format(Instant.parse(rev.getTimestamp()));
+            sb.append(formattedTime)
+                    .append(" - ")
+                    .append(rev.getUser())
                     .append("\n");
         }
         return sb.toString();
     }
 }
-
